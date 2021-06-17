65793a08db1a595e6166f54b584e2d69eb6447bb
gustavo <gusfer@gmail.com>
2021-06-17 12:38:29 +0200

    Networkx

--- generateTeiGraphml.py
+++ generateTeiGraphml.py
@@ -1,4 +1,5 @@
 import codecs 
+import networkx as nx 
 
 with codecs.open('./stemma.gv', 'r', 'utf-8') as infile:
     newContent = infile.read()
@@ -7,5 +8,13 @@ with codecs.open('./stemma.gv', 'r', 'utf-8') as infile:
 
     with codecs.open('./new_stemma.txt', 'w', 'utf-8') as outfile:
         outfile.write(newContent)
+
+G = nx.Graph()
+
+G.add_nodes_from([2, 3])
+
+nx.write_graphml(G,
+                 './new_stemma.graphml',
+                 encoding="utf-8")
 
         
