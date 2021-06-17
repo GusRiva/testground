00f168be56616fc6583c9dced9c09c43da414d63
gustavo <gusfer@gmail.com>
2021-06-17 13:06:21 +0200

    Prepare to work with changed files

--- generateTeiGraphml.py
+++ generateTeiGraphml.py
@@ -1,19 +1,22 @@
 import codecs 
 import networkx as nx 
+import sys 
 
-with codecs.open('./stemma.gv', 'r', 'utf-8') as infile:
+changed_file = str(sys.argv[1])
+
+with codecs.open(changed_file, 'r', 'utf-8') as infile:
     newContent = infile.read()
 
     newContent += "Resultado"
 
-    with codecs.open('./new_stemma.txt', 'w', 'utf-8') as outfile:
+    with codecs.open('./' + changed_file + '.txt', 'w', 'utf-8') as outfile:
         outfile.write(newContent)
 
 G = nx.Graph()
 
 G.add_nodes_from([2, 3])
 
-nx.write_graphml(G,'./new_stemma.graphml',
+nx.write_graphml(G,'./' + changed_file + '.graphml',
                  encoding="utf-8")
 
         
