f040f60e8b345fae247cccf99cb0e88e94e88b18
gustavo <gusfer@gmail.com>
2021-06-17 11:31:01 +0200

    First test

--- generateTeiGraphml.py
+++
@@ -1,9 +0,0 @@
-import codecs 
-
-with codecs.open('stemma.gv', 'r', 'utf-8') as infile:
-    newContent = infile.read()
-
-    newContent += "Resultado"
-
-    with codecs.open('new_stemma.txt', 'w', 'utf-8') as outfile:
-        outfile.write(newContent)
