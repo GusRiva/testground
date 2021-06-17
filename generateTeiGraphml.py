90490bff3055c1df5ec04e045a72008639164b31
gustavo <gusfer@gmail.com>
2021-06-17 14:45:33 +0200

    Using Networkx

--- generateTeiGraphml.py
+++ generateTeiGraphml.py
@@ -9,18 +9,6 @@ if len(sys.argv) > 1:
 else:
     changed_file = "/home/gustavo/Dokumente/OpenStemmata/test/Isopet.gv"
 
-# with codecs.open(changed_file, 'r', 'utf-8') as infile:
-#     newContent = infile.read()
-
-#     newContent += "Resultado"
-
-#     with codecs.open('./' + changed_file + '.txt', 'w', 'utf-8') as outfile:
-#         outfile.write(newContent)
-
-
-
-
-
 with codecs.open(changed_file, 'r', 'utf-8') as dotfile:
     lines = dotfile.readlines()
     nodes = {}
@@ -39,7 +27,14 @@ with codecs.open(changed_file, 'r', 'utf-8') as dotfile:
                 nodes[origin] = {}
             if dest not in nodes:
                 nodes[dest] = {}
-            edges.append((origin,dest))
+            edge_attr = {}
+            if '[' in noAttrib:
+                attributes = re.findall('(\w+)="(\w*)",?\s?', line)    
+                for attr in attributes:
+                    if attr[0] == 'style' and attr[1] == 'dashed':
+                        edge_attr['type'] = 'contamination'
+            edges.append((origin,dest, edge_attr))
+            
         elif '[' in noAttrib:
             node = re.split('\[', noAttrib)[0].strip()
             nodes[node] = {}
@@ -47,36 +42,44 @@ with codecs.open(changed_file, 'r', 'utf-8') as dotfile:
             for attr in attributes:
                 nodes[node][attr[0]] = attr[1]
 
-G = nx.Graph()
+nodes = [ (x,nodes[x]) for x in nodes ]
 
+G = nx.Graph()
+# print(edges)
 G.add_nodes_from(nodes)
 G.add_edges_from(edges)
 
+nx.write_graphml(G, changed_file[0:-3] + '.graphml', encoding="utf-8")
 
-# Modify the file extension
-nx.write_graphml(G, changed_file[0:-3] + '.graphml',
-                 encoding="utf-8")
+graph = et.Element('graph')
+tree = et.ElementTree(graph)
+graph.attrib['type'] = 'directed'
 
-
-
-graphEl = et.Element('graph')
-graph = et.ElementTree(graphEl)
-graphEl.attrib['type'] = 'directed'
-for node in nodes:
-    nodeEl = et.SubElement(graphEl, 'node', attrib={'{http://www.w3.org/XML/1998/namespace}id': node})
+for node in G.nodes(data=True):
+    # print(node)
+    nodeEl = et.SubElement(graph, 'node', attrib={'{http://www.w3.org/XML/1998/namespace}id': "n_" + node[0]})
     labelEl = et.SubElement(nodeEl, 'label')
-    if 'label' in nodes[node]:
-        label = nodes[node]['label']
+    if 'label' in node[1]:
+        label = node[1]['label']
         if not re.match(r'^\s*$', label):   
             labelEl.text = label   
-    if 'color' in nodes[node]:
-        color = nodes[node]['color']
+        else:
+            labelEl.text = ''  
+    else:
+        labelEl.text = node[0]  
+    if 'color' in node[1]:
+        color = node[1]['color']
         if color == 'grey':
             nodeEl.attrib['type'] = 'hypothetical'
         else:
             nodeEl.attrib['type'] = 'witness'
+    else:
+        nodeEl.attrib['type'] = 'witness'
             
-tree = et.ElementTree(graphEl)
+for edge in G.edges(data=True):
+    et.SubElement(graph, 'arc', attrib= {'from': "#n_" + edge[0], 'to': "#n_" + edge[1]})
+
+
 tree.write( changed_file[0:-3] + '.tei.xml', pretty_print=True, encoding="UTF-8", xml_declaration=True)
 
 
