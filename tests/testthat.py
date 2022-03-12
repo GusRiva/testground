59354e33f13eade65c28a05495f7a19a96088fd6
GusRiva <gusfer@gmail.com>
2022-03-12 18:27:40 +0100

    Adding more files for testing and subfolders test

--- tests/testthat.py
+++ tests/testthat.py
@@ -1,12 +1,11 @@
 import os 
 import glob 
-import unittest
 import sys
+import re 
 import csv
 import codecs 
 
 exit_code = 0
-error_place = ''
 
 class bcolors:
     HEADER = '\033[95m'
@@ -20,7 +19,8 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 #FOLDER STRUCTURE
-print('Testing folder structure...')
+print(f"{bcolors.HEADER}Testing folder structure...{bcolors.ENDC}")
+print('Check root folders and files...')
 valid_folder_structure =  [".git", ".gitignore", ".github", "data", "CITATION.cff", "examples", "example_graph.png", "LICENSE", "README.md",
             "schema", "tests", "transform"]
 actual_folder_structure = os.listdir('.')
@@ -30,10 +30,9 @@ for el in actual_folder_structure:
         if el not in valid_folder_structure:
             exit_code = 1
             folder_error = 1
-            error_place = el
             raise RuntimeError('')
     except:
-        print(f"{bcolors.FAIL}Error caused by directory <"+error_place+f">.{bcolors.ENDC}")
+        print(f"{bcolors.FAIL}Error caused by directory <"+el+f">.{bcolors.ENDC}")
         print(f"{bcolors.WARNING}Root folder structure should not be modified (submissions go in database/LANG/EDITOR_TITLE_DATE{bcolors.ENDC}")
         continue
 if folder_error == 0:
@@ -51,22 +50,30 @@ with codecs.open('./tests/testthat/iso-639-3_20200515.tab', 'r', 'utf-8') as iso
                 if part not in language_codes:
                     exit_code = 1
                     iso_error = 1
-                    error_place = folder
                     raise RuntimeError('')                
             except:
-                print(f"{bcolors.FAIL}Error caused by directory <"+error_place+f">.{bcolors.ENDC}")
+                print(f"{bcolors.FAIL}Error caused by directory <"+folder+f">.{bcolors.ENDC}")
                 print(f"{bcolors.WARNING}folder names should be ISO 639 language codes (with optional + symbol for language hybrids{bcolors.ENDC}")  
                 continue
 if iso_error == 0:
     print(f"{bcolors.OKGREEN}Folders follow the ISO language codes{bcolors.ENDC}")
 
-# test_that("data has correct subfolders named by ISO 639 language codes", {
-#   language_codes = read.csv("iso-639-3_20200515.tab", sep = "\t")
-#   observed = unique(unlist(strsplit(list.files("../../data/"), "\\+")))
-#   expect_true(all(observed %in% language_codes$Id), 
-#               info = "folder names should be ISO 639 language codes 
-#               (with optional + symbol for language hybrids")
-# })
+# FOLDER NAMING
+print("Checking that subsubfolders are named consistently (FirstEditorLastName_Date_TitleWord)")
+folders = [os.path.basename(x) for x in glob.iglob('data/*/*')]
+folder_name_error = 0
+for folder in folders:
+    try:
+        match= re.match('\w+_\d{1,4}[a-z]?_', folder)
+        if match is None:
+            folder_name_error = 1
+            exit_code = 1
+            raise RuntimeError('')
+    except:
+        print(f"{bcolors.FAIL}Error caused by directory <"+folder+f">.{bcolors.ENDC}")
+        continue
+if folder_name_error == 0:
+    print(f"{bcolors.OKGREEN}Subfolder are well constructed{bcolors.ENDC}")    
 
 # for file in glob.iglob('./data/*/*/*.tei.xml', recursive=True):
 #     print("Checking", file)
