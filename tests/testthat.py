732376e744d98e4115c3f2849c930496ca55638a
GusRiva <gusfer@gmail.com>
2022-03-12 16:51:47 +0100

    colors

--- tests/testthat.py
+++ tests/testthat.py
@@ -1,15 +1,27 @@
 import os 
 import glob 
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 try:
     print('Testing folder structure...')
     # valid_folder_structure = set("data", "CITATION.cff", "examples", "example_graph.png", "LICENSE", "README.md",
     #             "schema", "tests", "transform")
-    # actual_folder_structure = os.listdir('.')
+    actual_folder_structure = os.listdir('.')
+    print(actual_folder_structure)
 except:
     print('Root folder structure should not be modified (submissions go in database/LANG/EDITOR_TITLE_DATE')
 else:
-    print('Folder Structure Correct')
+    print(f"{bcolors.OKGREEN}Folder Structure Correct{bcolors.ENDC}")
 
 
 
