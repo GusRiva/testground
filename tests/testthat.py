147e4547c324bd84fbb7535280a0cb9d8e797b80
GusRiva <gusfer@gmail.com>
2022-03-12 16:48:21 +0100

    glob

--- tests/testthat.py
+++ tests/testthat.py
@@ -1,14 +1,17 @@
 import os 
+import glob 
 
 try:
-    print('Testing folder structure..')
-    valid_folder_structure = set("data", "CITATION.cff", "examples", "example_graph.png", "LICENSE", "README.md",
-                "schema", "tests", "transform")
-    actual_folder_structure = os.listdir('.')
+    print('Testing folder structure...')
+    # valid_folder_structure = set("data", "CITATION.cff", "examples", "example_graph.png", "LICENSE", "README.md",
+    #             "schema", "tests", "transform")
+    # actual_folder_structure = os.listdir('.')
 except:
     print('Root folder structure should not be modified (submissions go in database/LANG/EDITOR_TITLE_DATE')
 else:
     print('Folder Structure Correct')
 
-for file in os.listdir('./data/*/*/*'):
+
+
+for file in glob.iglob('./data/*/*/*.tei.xml', recursive=True):
     print("Checking", file)
