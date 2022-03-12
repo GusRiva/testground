3a1f5669aab2f8cad9c53fa00fa01cc691724d70
GusRiva <gusfer@gmail.com>
2022-03-12 17:31:13 +0100

    exist-code

--- tests/testthat.py
+++ tests/testthat.py
@@ -1,5 +1,9 @@
 import os 
 import glob 
+import unittest
+import sys
+
+exit_code = 0
 
 class bcolors:
     HEADER = '\033[95m'
@@ -12,20 +16,23 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
+# try:
+print('Testing folder structure...')
+valid_folder_structure =  [".git", ".gitignore", ".github", "data", "CITATION.cff", "examples", "example_graph.png", "LICENSE", "README.md",
+            "schema", "tests", "transform"]
+actual_folder_structure = os.listdir('.')
 try:
-    print('Testing folder structure...')
-    valid_folder_structure =  [".gitignore", ".github", "data", "CITATION.cff", "examples", "example_graph.png", "LICENSE", "README.md",
-                "schema", "tests", "transform"]
-    actual_folder_structure = os.listdir('.')
     for el in actual_folder_structure:
+        # unittest.assertIn(el, valid_folder_structure, f"{bcolors.WARNING}Unexprected file "+el+"{bcolors.ENDC}")
         if el not in valid_folder_structure:
-            raise RuntimeError(f"{bcolors.WARNING}Incorrect element: "+el+"{bcolors.ENDC}")
+            exit_code = 1
+            raise RuntimeError('')
 except:
-    print('Root folder structure should not be modified (submissions go in database/LANG/EDITOR_TITLE_DATE')
+    print(f"{bcolors.WARNING}Root folder structure should not be modified (submissions go in database/LANG/EDITOR_TITLE_DATE{bcolors.ENDC}")
 else:
     print(f"{bcolors.OKGREEN}Folder Structure Correct{bcolors.ENDC}")
 
-
-
 for file in glob.iglob('./data/*/*/*.tei.xml', recursive=True):
     print("Checking", file)
+
+sys.exit(exit_code)
